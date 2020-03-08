27d77c7960b73dd8810810e0fab72a220655bdb6
hanyijun <han.yijun@oyohotels.cn>
2020-03-08 15:29:36 +0800

    update

--- src/main/java/hanyijun/algorithm/Notify.java
+++ src/main/java/hanyijun/algorithm/Notify.java
@@ -5,40 +5,53 @@ package hanyijun.algorithm;
  * 启动两个线程, 一个输出 1,3,5,7…99, 另一个输出 2,4,6,8…100 最后 STDOUT 中按序输出 1,2,3,4,5…100
  */
 public class Notify {
-    private final Object flag = new Object();
+    private final Object lock = new Object();
+    private boolean isWait = false;
+
     public static void main(String[] args) throws Exception {
         Notify notify = new Notify();
-        ThreadA threadA = notify.new ThreadA();
-        threadA.start();
-        Thread.currentThread().sleep(1000);
         ThreadB threadB = notify.new ThreadB();
         threadB.start();
+        ThreadA threadA = notify.new ThreadA();
+        threadA.start();
     }
+
     class ThreadA extends Thread {
         @Override
         public void run() {
-            synchronized (flag) {
+            synchronized (lock) {
                 for (int i = 2; i <= 100; i += 2) {
                     try {
-                        flag.wait();
+                        isWait = true;
+                        lock.notify();
+                        lock.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     System.out.println(i);
-                    flag.notify();
+                    lock.notify();
                 }
             }
         }
     }
+
     class ThreadB extends Thread {
         @Override
         public void run() {
-            synchronized (flag) {
+            synchronized (lock) {
+                //为了确保ThreadA已经进入wait状态
+                while (!isWait) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
                 for (int i = 1; i < 100; i += 2) {
                     System.out.println(i);
-                    flag.notify();
+                    lock.notify();
                     try {
-                        flag.wait();
+                        lock.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
